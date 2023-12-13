95ae1f8b45f9a8fdc54d9b4ac17d00030e2687a9
ToluAjisola <toluwalopeajisola@gmail.com>
2023-12-13 15:46:39 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+    return (
+      <main className="bg-slate-300/20">
+        <Router>
+            <Navbar />
+          <Routes>
+            <Route path="/" element={'<Home />'} />
+            <Route path="/about" element={'<About />'} />
+          </Routes>
+        </Router>
+      </main>
+    );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+      
+          <div>Navbar</div>
+    
+  );
+};
+
+export default Navbar;
