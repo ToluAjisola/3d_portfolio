7db8a224c709bf9023858fa9862c3c6e7bcfcac7
ToluAjisola <toluwalopeajisola@gmail.com>
2023-12-16 20:12:03 -0500

    Navigation Bar creation

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ const App = () => {
     return (
       <main className="bg-slate-300/20">
         <Router>
-            <Navbar />
+          <Navbar />
           <Routes>
-            <Route path="/" element={'<Home />'} />
-            <Route path="/about" element={'<About />'} />
+            <Route path="/" element={"<Home />"} />
+            <Route path="/about" element={"<About />"} />
+            <Route path="/projects" element={"<Projects />"} />
+            <Route path="/contact" element={"<Contact />"} />
           </Routes>
         </Router>
       </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,33 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-      
-          <div>Navbar</div>
-    
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white
+      items-center justify-center flex "
+      >
+        <p className="blue-gradient_text font-bold shadow-md">TA</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "text-black"
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/projects"
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "text-black"
+            }
+          >
+            Projects
+          </NavLink>
+      </nav>
+    </header>
   );
 };
 
