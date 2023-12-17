04082daa2cec264c03c410bce035f3176ac30c39
ToluAjisola <toluwalopeajisola@gmail.com>
2023-12-16 20:39:34 -0500

    Route implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import Home from "./pages/Home";
 
 const App = () => {
     return (
@@ -8,10 +12,10 @@ const App = () => {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={"<Home />"} />
-            <Route path="/about" element={"<About />"} />
-            <Route path="/projects" element={"<Projects />"} />
-            <Route path="/contact" element={"<Contact />"} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </Router>
       </main>
