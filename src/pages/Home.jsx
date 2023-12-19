c1b1049588f5426b3f6e49c6b2acf0008d372090
ToluAjisola <toluwalopeajisola@gmail.com>
2023-12-18 23:40:58 -0500

    Lighting adjustment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,8 @@ const Home = () => {
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
-      let screenPosition = [0, -6.5, -43.4];
+    let screenPosition = [0, -6.5, -43.4];
+     let rotaion = [0.1, 4.7, 0];
     
     if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
@@ -16,10 +17,11 @@ const Home = () => {
       screenScale = [1, 1, 1];
     }
 
-    return [screenScale, screenPosition];
+    return [screenScale, screenPosition, rotaion];
   };
 
-  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
 
     return (
       <section className="w-full h-screen relative">
@@ -30,15 +32,20 @@ const Home = () => {
           camera={{ near: 0.1, far: 1000 }}
         >
           <Suspense fallback={<Loader />}>
-            <directionalLight />
-            <ambientLight />
-            <pointLight />
+            <directionalLight position={[1, 1, 1]} intensity={2} />
+            <ambientLight intensity={0.5} />
+            {/* or <pointLight position={[10, 5, 10]} intensity={2} /> if no directional or ambient light */}
             <spotLight />
-            <hemisphereLight />
+            <hemisphereLight
+              skyColor="#b1e1ff"
+              groundColor="#000000"
+              intensity={0}
+            />
 
             <Island
               position={islandPosition}
               scale={islandScale}
+              rotation={islandRotation}
             />
           </Suspense>
         </Canvas>
