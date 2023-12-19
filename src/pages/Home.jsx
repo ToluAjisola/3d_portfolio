2b3bd8027b61ee2c648d054aec36f9a16e5274c7
ToluAjisola <toluwalopeajisola@gmail.com>
2023-12-18 19:37:04 -0500

    Adjust island scaling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,22 @@ import Loader from '../components/Loader';
 import Island from '../models/Island';
 
 const Home = () => {
+
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+      let screenPosition = [0, -6.5, -43.4];
+    
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+
     return (
       <section className="w-full h-screen relative">
         {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
@@ -16,11 +32,14 @@ const Home = () => {
           <Suspense fallback={<Loader />}>
             <directionalLight />
             <ambientLight />
-            <pointLight  />
-            <spotLight/>
-                    <hemisphereLight />
-                    
-                    <Island />
+            <pointLight />
+            <spotLight />
+            <hemisphereLight />
+
+            <Island
+              position={islandPosition}
+              scale={islandScale}
+            />
           </Suspense>
         </Canvas>
       </section>
