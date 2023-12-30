38216b657598b89163829fb671804ef29205ea83
ToluAjisola <toluwalopeajisola@gmail.com>
2023-12-29 22:42:05 -0500

    Added Emailjs to send emails on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react'
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const formRef = useRef();
@@ -13,7 +14,29 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setLoading(true);
+    setIsLoading(true);
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Toluwalope Ajisola",
+        from_email: form.email,
+        to_email: "ajisola.toluwalope@gmail.com",
+        message: form.message,
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+      //Success message
+       setForm({name: "", email: "", message: "",
+       });
+    }).catch((error) => {
+      setIsLoading(false);
+      console.log(error);
+      //Show alert
+    })
+      ;
   };
 
   return (
