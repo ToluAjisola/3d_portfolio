14b4ecc327e4c0ac365290be5a902611c6c397e9
ToluAjisola <toluwalopeajisola@gmail.com>
2024-01-02 23:12:59 -0500

    Added About page into

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          {" "}
+          Tolu
+        </span>{" "}
+      </h1>
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+        Bachelor's Degree and experience working in a company. I picked up many
+          skills along the way
+          </p>
+      </div>
+    </section>
+  );
 }
 
 export default About
