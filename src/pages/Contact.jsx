b4b5c960a591a1fa7de6f05ab07b1d1144bc15d3
ToluAjisola <toluwalopeajisola@gmail.com>
2024-11-04 15:31:35 -0500

    Test complete

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,9 +19,9 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
-    console.log("Service ID:", import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
+    /* console.log("Service ID:", import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
     console.log("Template ID:", import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID);
-    console.log("Public Key:", import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
+    console.log("Public Key:", import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY); */
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
