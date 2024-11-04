7320b26bb93b4eb4a85937eac089b30f88bb068b
ToluAjisola <toluwalopeajisola@gmail.com>
2024-11-04 16:30:56 -0500

    Email testing

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -28,9 +28,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Toluwalope Ajisola",
           from_email: form.email,
-          to_email: "ajisola.toluwalope@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY ,
